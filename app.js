615fc0e33e6cb6aeac32fe7859fe4b96669a652c
emdad <emdad.hossain321@gmail.com>
2018-09-15 23:23:51 +0600

    `${user.username}` notation in cmment

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 console.log('starting app.');
 
 const fs = require('fs');
+const os = require('os');
 
+var user = os.userInfo();
+
+console.log(user);
+fs.appendFile('greetings.txt',`Hello ${user.username}!`,function (err) {
+    if (err)
+        console.log('unable to write to file!');
+});
