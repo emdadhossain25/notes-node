00bbe23bf466a936d79beeffd813c51c59a2aab6
emdad <emdad.hossain321@gmail.com>
2018-09-15 23:56:22 +0600

    added custom node module file notes.js

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-console.log('starting app.');
+console.log('starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const notes = require('./notes.js');
 
 var user = os.userInfo();
 
