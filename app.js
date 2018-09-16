0cc02b146f5fa8524310210d8ee1b96d5cd6979d
Emdad Hossain <emdad.hossain321@gmail.com>
2018-09-16 13:16:18 +0600

    added nodemon videos

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
 console.log('starting app.js');
 
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
+
+
 const notes = require('./notes.js');
 
 var res = notes.addNote();
 console.log(res);
 
-var sum = notes.add(6,4);
-console.log(sum);
+
 
 console.log(_.isString(true));
 console.log(_.isString('emdad'));
 
-
-var user = os.userInfo();
 
 
 
@@ -23,8 +21,3 @@ var user = os.userInfo();
 var filteredArray = _.uniq(['emdad',1,'emdad',1,2,3,4,5])
 console.log(filteredArray);
 
-console.log(user);
-fs.appendFile('greetings.txt',`Hello ${user.username}! `,function (err) {
-    if (err)
-        console.log('unable to write to file!');
-});
--- notes.js
+++ notes.js
@@ -5,8 +5,3 @@ module.exports.addNote = ()=>{
     console.log('addNote');
     return 'new note'
 }
-
-module.exports.add = (a, b)=>{
-    console.log('adding number');
-    return a+b;
-}
