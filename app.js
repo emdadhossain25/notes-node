7f1a50e90772d562604d81103e6a43c16868cd6d
Emdad Hossain <emdad.hossain321@gmail.com>
2018-09-16 12:23:03 +0600

    added lodash

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ console.log('starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
 
 var res = notes.addNote();
@@ -10,11 +11,16 @@ console.log(res);
 var sum = notes.add(6,4);
 console.log(sum);
 
+console.log(_.isString(true));
+console.log(_.isString('emdad'));
 
-// var user = os.userInfo();
 
-// console.log(user);
-// fs.appendFile('greetings.txt',`Hello ${user.username}! `,function (err) {
-//     if (err)
-//         console.log('unable to write to file!');
-// });
+var user = os.userInfo();
+
+
+
+console.log(user);
+fs.appendFile('greetings.txt',`Hello ${user.username}! `,function (err) {
+    if (err)
+        console.log('unable to write to file!');
+});
