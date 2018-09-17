65a4b5479e3bc0c29296a2e7a461688406cd49fb
Emdad Hossain <emdad.hossain321@gmail.com>
2018-09-17 12:59:22 +0600

    added note adding function

--- notes.js
+++ notes.js
@@ -1,14 +1,37 @@
 console.log('starting notes.js')
 
-var addNote = (title, body)=>{
-    console.log('adding node', title,body);
+const fs = require('fs');
+
+var addNote = (title, body) => {
+    var notes = [];
+    var note = {
+        title: title,
+        body: body
+    }
+
+    try {
+        var notesString = fs.readFileSync('notes-data.json');
+        notes = JSON.parse(notesString);
+    } catch (e) {
+
+    }
+
+    var duplicateNotes = notes.filter((note) => note.title === title);
+
+    if (duplicateNotes.length === 0) {
+
+        notes.push(note);
+        fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+    }
+
+
 };
 
 
 /**
  * function for getting all logs
  */
-var getAll = () =>{
+var getAll = () => {
 
     console.log('getting all log')
 };
@@ -17,23 +40,22 @@ var getAll = () =>{
 /**
  * function for calling getting read
  */
-var getNote = (title) =>{
-    console.log('getting note',title);
+var getNote = (title) => {
+    console.log('getting note', title);
 }
-
 
 
 /**
  * function for removing methods
  */
-var removeNote = (title) =>{
-    console.log('removing note',title);
+var removeNote = (title) => {
+    console.log('removing note', title);
 }
 
 
 module.exports = {
-    addNote : addNote,
-    getAll : getAll,
-    getNote : getNote,
-    removeNote : removeNote
+    addNote: addNote,
+    getAll: getAll,
+    getNote: getNote,
+    removeNote: removeNote
 }
